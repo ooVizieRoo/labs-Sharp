07b28dd380027fc22e12fad3433694089b5bbaae
ooVizieRoo <vizier.khv@gmail.com>
2015-09-20 19:40:59 +1000

    20.09.2015
    Выполнено до разделения по вариантам

--- prakt_1/prakt_1/Person.cs
+++ prakt_1/prakt_1/Person.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace prakt_1
 {
+    interface IDateAndCopy
+    {
+        object DeepCopy();
+        DateTime Date { get; set; }
+    }
+
     class Person
     {
         private string Name;
@@ -81,14 +87,46 @@ namespace prakt_1
         }
 #endregion
 
+        #region Вывод  значений полей класса
+        //Всех
         public override string ToString()
         {
             return "Имя: " + Name.ToString() + "; Фамилия: " + Family.ToString() + "; Дата Рождения: " + BirthDate.ToString();
         }
 
+        //Только имя и фамилия
         public virtual string ToShortString()
         {
             return "Имя: " + Name.ToString() + "; Фамилия: " + Family.ToString();
+        }
+        #endregion
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Person p = obj as Person;
+            if (p == null)
+                return false;
+
+
+            return (p.Name == this.Name) && (p.Family == this.Family) && (p.BirthDate == this.BirthDate);
+        }
+
+        public static bool operator ==(Person p1, Person p2)
+        {
+            return p1.Equals(p2);
+        }
+
+        public static bool operator !=(Person p1, Person p2)
+        {
+            return !p1.Equals(p2);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
         }
     }
 }
