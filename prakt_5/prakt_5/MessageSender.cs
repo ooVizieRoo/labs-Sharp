94fd8b24206cbfcd32bb66569df8aea03593e32d
ooVizieRoo <vizier.khv@gmail.com>
2015-12-01 02:07:30 +1000

    начал

---
+++ prakt_5/prakt_5/MessageSender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt_5
+{
+    class MessageSender
+    {
+        private int Send(byte[] buf)
+        {
+            return buf.Length;
+        }
+
+        public void SendMessage(string msg)
+        {
+            byte[] buf;
+
+            buf = new byte[msg.Length * sizeof(char)];
+            Buffer.BlockCopy(msg.ToCharArray(), 0, buf, 0, buf.Length);
+
+            Send(buf);
+        }
+    }
+}
