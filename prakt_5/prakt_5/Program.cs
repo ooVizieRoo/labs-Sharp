562cd9abe28f4aab6cabdea306303a52bc5ad08b
ooVizieRoo <vizier.khv@gmail.com>
2015-12-11 01:15:02 +1000

    wtf

--- prakt_5/prakt_5/Program.cs
+++ prakt_5/prakt_5/Program.cs
@@ -6,6 +6,25 @@ using System.Threading.Tasks;
 
 namespace prakt_5
 {
+    /*
+        interface Window {}
+        class XWindow : Window {}
+        class WaylandWindow : Window {}
+        interface IconWindow : Window {}
+        class XIconWindow : XWindow, IconWindow {}
+        class WaylandIconWindow : WaylandWindow, IconWindow {}
+        interface ModalWindow : Window {}
+        class XModalWIndow : XWindow, ModalWindow {}
+        class WaylandModalWindow : WaylandWindow, ModalWindow {}
+     */
+    interface Window { }
+    class IconWindow : Window { /*ссылка на WindowImplementor*/ }
+    class ModalWindow : Window { /*ссылка на WindowImplementor*/ }
+    interface WindowImplementor : Window { }
+    class XWindowImplementor : WindowImplementor { }
+    class WaylandImplementor : WindowImplementor { }
+
+
     class Program
     {
         static void Main(string[] args)
